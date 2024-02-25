0067c2d603c2eb7db67be5ec9bb665bcdd5d9059
Nathan Kerns <nmk456@gmail.com>
2024-02-25 14:44:06 -0800

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name="openlst-sw",
     version='0.1',
-    packages=["openlst_tools"]
+    packages=["openlst_tools"],
+    install_requires=["pyserial", "numpy", "ipython"]
 )
