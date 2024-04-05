0d89d2f324e581b9273c87c89bc9b3c0113f82e7
Nathan Kerns <nmk456@gmail.com>
2024-04-05 13:00:52 -0400

    BER script mostly working

--- openlst_tools/ber_lst.py
+++ openlst_tools/ber_lst.py
@@ -39,17 +39,15 @@ class BerTester:
         del self.lst_r
         del self.lst_t
 
-    def reboot(self, size, receive=True, transmit=True):
+    def reboot(self, receive=True, transmit=True):
         if receive:
             self.lst_r.reboot()
         
         if transmit:
             self.lst_t.reboot()
 
-        # Wait for 15 packets worth of time to make uptime valid
-        # time.sleep(max(size*8*15/(self.drate/2), 1))
-        time.sleep(1.5)
-        # lst.set_rf_params(drate=self.drate, deviation=self.dev, chan_bw=self.bw)
+        # Breaks without this sleep
+        time.sleep(3)
 
     def set_params(self, rate, gain):
         _, self.bw, self.drate, self.dev = self.lst_r.set_rf_params(drate=rate)
@@ -67,36 +65,41 @@ class BerTester:
         self.reboot(size)
         self.set_params(rate, gain)
 
-        print(f"Using datarate={int(self.drate)}, deviation={int(self.dev)}, bandwidth={int(self.bw)}")
+        # print(f"Using datarate={int(self.drate)}, deviation={int(self.dev)}, bandwidth={int(self.bw)}")
 
-        last_uptime = self.lst_r.get_telem()["uptime"]
-        last_packets = 0
+        telem = self.lst_r.get_telem()
+        last_uptime = telem["uptime"]
+        last_packets = telem["packets_good"]
         good_packets = 0
+
+        rssi = 0
 
         i = 0
 
+        # Breaks without this sleep and the sleep in self.reboot
+        time.sleep(3)
+
         while i < count:
             for _ in range(PACKET_STEP):
-                # If we send it to the HWID of the OpenLST, it will just drop
-                # the message instead of forwarding it. This might help reduce
-                # errors.
-                self.lst_t.transmit(b'0'*(size-10), self.hwid_r)
-                time.sleep(5*size*8*2/self.drate)
+                self.lst_t.transmit(b'0'*(size-10))
+                time.sleep(size*8*2/self.drate)
 
             # Wait for packet to finish transmitting
-            time.sleep(10)
+            time.sleep(1)
 
             try:
                 telem = self.lst_r.get_telem()
             except (AssertionError, TimeoutError):
                 telem = None
 
-            print(telem["packets_good"], telem["cs_count"], telem["sfd_count"], telem["rssi_dbm"])
+            # print(telem["packets_good"], telem["cs_count"], telem["sfd_count"], telem["rssi_dbm"])
 
             if telem is not None and telem["uptime"] >= last_uptime:
                 i += PACKET_STEP
                 new_packets = telem["packets_good"] - last_packets
                 good_packets += new_packets
+
+                rssi += telem["rssi_dbm"] / (count/PACKET_STEP)
 
                 last_uptime = telem["uptime"]
                 last_packets = telem["packets_good"]
@@ -110,9 +113,7 @@ class BerTester:
 
             self.lst_r.clean_packets()
 
-        time.sleep(1)
-
-        return 1 - good_packets / count
+        return 1 - good_packets / count, rssi
 
 # table 72 pg 207
 powers = [-30, -20, -15, -10, -5, 0, 5, 7, 10]
@@ -125,23 +126,25 @@ def vary_gain(port_r, port_t, hwid_r=0x00, hwid_t=0x01, num=4000, min=-30, max=1
     gains = [p for p in powers if min <= p <= max]
 
     pers = np.zeros(len(gains_s), dtype=np.float32)
+    rssis = np.zeros(len(gains_s), dtype=np.float32)
 
     for i in range(len(gains)):
-        per = tester.packet_error_rate(num, gains_s[i], size, rate=rate)
+        per, rssi = tester.packet_error_rate(num, gains_s[i], size, rate=rate)
 
         pers[i] = per
+        rssis[i] = rssi
 
-        print(f"G={gains[i]}, PER={per}")
+        print(f"G={gains[i]}, PER={per}, RSSI={rssi}")
 
-    final_data = np.array([gains, pers]).T
-    # np.savetxt(f'gain_vs_received_n={num}_min={min}_max={max}_r={int(rate)}.csv', final_data, delimiter=',')
+    final_data = np.array([gains, rssis]).T
+    np.savetxt(f'gain_vs_received_n={num}_min={min}_max={max}_r={int(rate)}.csv', final_data, delimiter=',')
 
-    # plt.semilogy(gains, pers)
-    # plt.xlabel("SDR Gain (dB)")
-    # plt.ylabel("PER")
-    # plt.title(f"Packet Error Rate N={num}, R={int(rate)}, S={size}")
+    plt.semilogy(gains, rssis)
+    plt.xlabel("RSSI (dBm)")
+    plt.ylabel("PER")
+    plt.title(f"Packet Error Rate N={num}, R={int(rate)}, S={size}")
 
-    # plt.savefig(f'gain_vs_received_n={num}_min={min}_max={max}_r={int(rate)}.png')
+    plt.savefig(f'gain_vs_received_n={num}_min={min}_max={max}_r={int(rate)}.png')
 
 if __name__ == "__main__":
-    vary_gain(port_t='/dev/ttyUSB0', port_r='/dev/ttyUSB1', hwid_t=0x0071, hwid_r=0x0171, num=50, min=-20, max=-10, size=20, rate=38.4e3)
+    vary_gain(port_t='/dev/ttyUSB0', port_r='/dev/ttyUSB1', hwid_t=0x0071, hwid_r=0x0171, num=1000, min=-30, max=-10, size=20, rate=38.4e3)
